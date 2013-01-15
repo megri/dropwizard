383389fb95471a3a7d9ee3f1f98acf4268e226c5
megri <teknocide@gmail.com>
2013-01-16 00:48:15 +0100

    trying a better way of detecting file encoding

--- dropwizard-views/src/main/java/com/yammer/dropwizard/views/freemarker/FreemarkerViewRenderer.java
+++ dropwizard-views/src/main/java/com/yammer/dropwizard/views/freemarker/FreemarkerViewRenderer.java
@@ -11,27 +11,81 @@ import freemarker.template.Configuration;
 import freemarker.template.DefaultObjectWrapper;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
+import org.mozilla.universalchardet.UniversalDetector;
 
 import javax.ws.rs.WebApplicationException;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.util.Locale;
 
 public class FreemarkerViewRenderer implements ViewRenderer {
-    private static class TemplateLoader extends CacheLoader<Class<?>, Configuration> {
+    private static class ViewClass {
+        private final Class<?> klass;
+        private final View view;
+
+        public ViewClass(Class<?> klass, View view) {
+            this.klass = klass;
+            this.view = view;
+        }
+
         @Override
-        public Configuration load(Class<?> key) throws Exception {
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            ViewClass viewClass = (ViewClass) o;
+
+            if (!klass.equals(viewClass.klass)) return false;
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            return klass.hashCode();
+        }
+    }
+    private static class TemplateLoader extends CacheLoader<ViewClass, Configuration> {
+        @Override
+        public Configuration load(ViewClass viewClass) throws Exception {
             final Configuration configuration = new Configuration();
             configuration.setObjectWrapper(new DefaultObjectWrapper());
-            configuration.loadBuiltInEncodingMap();
-            configuration.setDefaultEncoding(Charsets.UTF_8.name());
-            configuration.setClassForTemplateLoading(key, "/");
+            String templateName = viewClass.view.getTemplateName();
+            String encoding = detectViewEncoding(templateName);
+            if (encoding == null) {
+                configuration.loadBuiltInEncodingMap();
+                configuration.setDefaultEncoding(Charsets.UTF_8.name());
+            } else {
+                configuration.setDefaultEncoding(encoding);
+            }
+            configuration.setClassForTemplateLoading(viewClass.klass, "/");
             return configuration;
+        }
+
+        private static String detectViewEncoding(String viewName) {
+            String encoding;
+
+            try {
+                byte[] buf = new byte[4096];
+                java.io.FileInputStream fis = new java.io.FileInputStream(viewName);
+                UniversalDetector detector = new UniversalDetector(null);
+                int nread;
+                while ((nread = fis.read(buf)) > 0 && !detector.isDone()) {
+                    detector.handleData(buf, 0, nread);
+                }
+                detector.dataEnd();
+                encoding = detector.getDetectedCharset();
+            } catch (IOException e) {
+                encoding = null;
+            }
+
+            return encoding;
         }
     }
 
-    private final LoadingCache<Class<?>, Configuration> configurationCache;
+    private final LoadingCache<ViewClass, Configuration> configurationCache;
 
     public FreemarkerViewRenderer() {
         this.configurationCache = CacheBuilder.newBuilder()
@@ -49,11 +103,9 @@ public class FreemarkerViewRenderer implements ViewRenderer {
                        Locale locale,
                        OutputStream output) throws IOException, WebApplicationException {
         try {
-            final Configuration configuration = configurationCache.getUnchecked(view.getClass());
-            String encoding = System.getProperty("file.encoding");
-            final Template template = encoding == null || encoding.isEmpty()?
-                configuration.getTemplate(view.getTemplateName(), locale) :
-                configuration.getTemplate(view.getTemplateName(), locale, encoding);
+            ViewClass viewClass = new ViewClass(view.getClass(), view);
+            final Configuration configuration = configurationCache.getUnchecked(viewClass);
+            final Template template = configuration.getTemplate(view.getTemplateName(), locale);
             template.process(view, new OutputStreamWriter(output, template.getEncoding()));
         } catch (TemplateException e) {
             throw new ContainerException(e);
